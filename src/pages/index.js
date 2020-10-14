57611dd238e5f258acb579e0de2edaa0ec435585
alex0112 <alarsen0112@gmail.com>
2020-10-14 00:43:53 -0600

    Oh yeah, its all comin together

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,19 +4,61 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import {Container, Row, Col, Button} from "react-bootstrap"
+
+
+const project_data = [
+    {
+	name: "Yggdrasil",
+	subtext: "Yggdrasil is a fun little side project I've been working on for...",
+	img_path: '../images/yggdrasil.jpg',
+    },
+    {
+	name: "Yggdrasil",
+	subtext: "Yggdrasil is a fun little side project I've been working on for...",
+	img_path: '../images/yggdrasil.jpg',
+    },
+
+    {
+	name: "Yggdrasil",
+	subtext: "Yggdrasil is a fun little side project I've been working on for...",
+	img_path: '../images/yggdrasil.jpg',
+    },
+
+    {
+	name: "Yggdrasil",
+	subtext: "Yggdrasil is a fun little side project I've been working on for...",
+	img_path: '../images/yggdrasil.jpg',
+    },
+
+    {
+	name: "Yggdrasil",
+	subtext: "Yggdrasil is a fun little side project I've been working on for...",
+	img_path: '../images/yggdrasil.jpg',
+    },
+];
+
+const ProjectRow = () => (
+    <Row>
+	
+    </Row>
+)
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
+    // <Layout>
+    // 	<SEO title="Alex Larsen" />
+
+    <Container fluid>
+	<Button variant="danger">Danger</Button>
+
+	<Row>
+	    <Col xs={1} sm={2} md lg className="square">1 of 3</Col>
+	    <Col xs={1} sm={2} md lg className="square">1 of 3</Col>
+	    <Col xs={1} sm={2} md lg className="square">1 of 3</Col>
+	</Row>
+
+    </Container>
+//    </Layout>
 )
 
 export default IndexPage
