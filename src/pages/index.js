cc1a4454a1242df1d7ffe4574b66819495c077d7
alex0112 <alarsen0112@gmail.com>
2020-10-14 16:06:43 -0600

    Got the hero working like I wanted. Sort of

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,47 +12,162 @@ const project_data = [
 	name: "Yggdrasil",
 	subtext: "Yggdrasil is a fun little side project I've been working on for...",
 	img_path: '../images/yggdrasil.jpg',
+	page: "/yggdrasil",
     },
     {
 	name: "Yggdrasil",
 	subtext: "Yggdrasil is a fun little side project I've been working on for...",
 	img_path: '../images/yggdrasil.jpg',
+	page: "/yggdrasil",
     },
-
     {
 	name: "Yggdrasil",
 	subtext: "Yggdrasil is a fun little side project I've been working on for...",
 	img_path: '../images/yggdrasil.jpg',
+	page: "/yggdrasil",
     },
-
     {
 	name: "Yggdrasil",
 	subtext: "Yggdrasil is a fun little side project I've been working on for...",
 	img_path: '../images/yggdrasil.jpg',
+	page: "/yggdrasil",
     },
-
     {
 	name: "Yggdrasil",
 	subtext: "Yggdrasil is a fun little side project I've been working on for...",
 	img_path: '../images/yggdrasil.jpg',
+	page: "/yggdrasil",
+    },
+    {
+	name: "Yggdrasil",
+	subtext: "Yggdrasil is a fun little side project I've been working on for...",
+	img_path: '../images/yggdrasil.jpg',
+	page: "/yggdrasil",
+    },
+    {
+	name: "Yggdrasil",
+	subtext: "Yggdrasil is a fun little side project I've been working on for...",
+	img_path: '../images/yggdrasil.jpg',
+	page: "/yggdrasil",
+    },
+    {
+	name: "Yggdrasil",
+	subtext: "Yggdrasil is a fun little side project I've been working on for...",
+	img_path: '../images/yggdrasil.jpg',
+	page: "/yggdrasil",
+    },
+    {
+	name: "Yggdrasil",
+	subtext: "Yggdrasil is a fun little side project I've been working on for...",
+	img_path: '../images/yggdrasil.jpg',
+	page: "/yggdrasil",
+    },
+    {
+	name: "Yggdrasil",
+	subtext: "Yggdrasil is a fun little side project I've been working on for...",
+	img_path: '../images/yggdrasil.jpg',
+	page: "/yggdrasil",
+    },
+    {
+	name: "Yggdrasil",
+	subtext: "Yggdrasil is a fun little side project I've been working on for...",
+	img_path: '../images/yggdrasil.jpg',
+	page: "/yggdrasil",
+    },
+    {
+	name: "Yggdrasil",
+	subtext: "Yggdrasil is a fun little side project I've been working on for...",
+	img_path: '../images/yggdrasil.jpg',
+	page: "/yggdrasil",
+    },
+    {
+	name: "Yggdrasil",
+	subtext: "Yggdrasil is a fun little side project I've been working on for...",
+	img_path: '../images/yggdrasil.jpg',
+	page: "/yggdrasil",
+    },
+    {
+	name: "Yggdrasil",
+	subtext: "Yggdrasil is a fun little side project I've been working on for...",
+	img_path: '../images/yggdrasil.jpg',
+	page: "/yggdrasil",
+    },
+    {
+	name: "Yggdrasil",
+	subtext: "Yggdrasil is a fun little side project I've been working on for...",
+	img_path: '../images/yggdrasil.jpg',
+	page: "/yggdrasil",
+    },
+    {
+	name: "Yggdrasil",
+	subtext: "Yggdrasil is a fun little side project I've been working on for...",
+	img_path: '../images/yggdrasil.jpg',
+	page: "/yggdrasil",
+    },
+    {
+	name: "Yggdrasil",
+	subtext: "Yggdrasil is a fun little side project I've been working on for...",
+	img_path: '../images/yggdrasil.jpg',
+	page: "/yggdrasil",
+    },
+    {
+	name: "Yggdrasil",
+	subtext: "Yggdrasil is a fun little side project I've been working on for...",
+	img_path: '../images/yggdrasil.jpg',
+	page: "/yggdrasil",
+    },
+    {
+	name: "Yggdrasil",
+	subtext: "Yggdrasil is a fun little side project I've been working on for...",
+	img_path: '../images/yggdrasil.jpg',
+	page: "/yggdrasil",
+    },
+    {
+	name: "Yggdrasil",
+	subtext: "Yggdrasil is a fun little side project I've been working on for...",
+	img_path: '../images/yggdrasil.jpg',
+	page: "/yggdrasil",
     },
 ];
 
 const ProjectRow = ({projects}) => {
   return projects.map((project) =>
-	<Col xs={1} sm={2} md lg className="square">{project.name}</Col>);
+      <Col xs={1} sm={2} md={3} lg={3} className="square">
+	  <ProjectTile project={project}/>
+      </Col>
+  );
 };
+
+const ProjectTile = ({project}) => {
+    return(
+	<a href={project.page}>
+	    <div>
+		<h4>{project.name}</h4>
+		<p>{project.subtext}</p>
+	    </div>
+	</a>
+    );
+}
 
 const IndexPage = () => (
     // <Layout>
     // 	<SEO title="Alex Larsen" />
-
-    <Container fluid>
-	<Button variant="warning">Button</Button>
-	<Row>
-	    <ProjectRow projects={project_data} />
+    <Container fluid id="main-container">
+	<Row id="hero" className="justify-content-center">
+	    <span id="call-buttons">
+		<Col xs={1} sm={2} md={2} lg={2}>
+		    <Button variant="warning" className="hero-button">Hire Me</Button>
+		</Col>
+		
+		<Col xs={1} sm={2} md={2} lg={2}>
+		    <Button variant="warning" className="hero-button">See My Work</Button>
+		</Col>
+	    </span>
 	</Row>
 
+	<Row id="projects" className="justify-content-center">
+	    <ProjectRow projects={project_data} />
+	</Row>
     </Container>
 //    </Layout>
 )
