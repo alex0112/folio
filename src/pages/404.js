0be121135cd7d5b9542207c058e48334237b168a
alex0112 <alarsen0112@gmail.com>
2022-06-16 19:17:10 -0600

    pretter 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,8 +6,8 @@ import SEO from "../components/seo"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1 id="404-title" className="hero-text">404: Not Found</h1>
+    <p id="404-text">You just hit a route that doesn&#39;t exist... the sadness.</p>
   </Layout>
 )
 
