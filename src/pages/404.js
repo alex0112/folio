40a3938e34a4c350aabfeb24ce4aa32d114e1ad5
alex0112 <alarsen0112@gmail.com>
2022-06-16 20:00:21 -0600

    additional styling 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,11 +4,11 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const NotFoundPage = () => (
-    <>
+    <section id="not-found-container"
 	<SEO title="404: Not found" />
 	<h1 id="not-found-title" className="hero-text">404: Not Found</h1>
 	<p id="not-found-text">Lost are we?</p>
-    </>
+    <section>
 )
 
 export default NotFoundPage
