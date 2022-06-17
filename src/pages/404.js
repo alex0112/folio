66c196a8b000303d6c03244635f3471e1275bfc3
alex0112 <alarsen0112@gmail.com>
2022-06-16 20:15:29 -0600

    forgot a slash

--- src/pages/404.js
+++ src/pages/404.js
@@ -8,7 +8,7 @@ const NotFoundPage = () => (
 	<SEO title="404: Not found" />
 	<h1 id="not-found-title" className="hero-text">404: Not Found</h1>
 	<p id="not-found-text">Lost are we?</p>
-    <section>
+    </section>
 )
 
 export default NotFoundPage
