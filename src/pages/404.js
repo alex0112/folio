a9e95e3c68580d5475deb825578424ddb66b72c6
alex0112 <alarsen0112@gmail.com>
2022-06-16 19:30:29 -0600

    404 styling

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,11 +4,9 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const NotFoundPage = () => (
-  <Layout>
     <SEO title="404: Not found" />
-    <h1 id="404-title" className="hero-text">404: Not Found</h1>
-    <p id="404-text">You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+    <h1 id="not-found-title" className="hero-text">404: Not Found</h1>
+    <p id="not-found-text">You just hit a route that doesn&#39;t exist... the sadness.</p>
 )
 
 export default NotFoundPage
